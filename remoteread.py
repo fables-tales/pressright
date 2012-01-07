043d11322d2530ae1fa14f1142cd1d69342e689b
Sam Phippen <samphippen@googlemail.com>
2012-01-07 13:01:06 +0000

    Made remotemread support going left as well as right
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- remoteread.py
+++ remoteread.py
@@ -8,5 +8,10 @@ def main():
         if data == "0x18 depressed":
             right = subprocess.Popen(["./right"])
             right.wait()
+
+        if data == "0x19 depressed":
+            left = subprocess.Popen(["./left"])
+            left.wait()
+
 if __name__ == "__main__":
     main()
