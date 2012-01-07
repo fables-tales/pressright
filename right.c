37ad1d14457805aa87be3e9a05eaa33e888c71d3
Sam Phippen <samphippen@googlemail.com>
2012-01-07 12:57:45 +0000

    Cleaned up right.c a little
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- right.c
+++ right.c
@@ -1,11 +1,11 @@
 #include <ApplicationServices/ApplicationServices.h>
 
 int main() {
-CGEventRef event1, event2, event3, event4;
-event1 = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)124, true);
-event4 = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)124, false);
-CGEventTapLocation loc =    kCGSessionEventTap;
-CGEventPost(loc, event1);
-CGEventPost(loc, event4);
+    CGEventRef event1, event2;
+    event1 = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)124, true);
+    event2 = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)124, false);
+    CGEventTapLocation loc = kCGSessionEventTap;
+    CGEventPost(loc, event1);
+    CGEventPost(loc, event2);
 return 0;
 }
